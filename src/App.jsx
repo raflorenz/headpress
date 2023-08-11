67420dd5332346a68d1c8cfc98c089b6f8667815
Raffy Lorenzana <rjlorenz931@gmail.com>
2023-08-11 22:52:20 +0800

    fetch posts from wptavern site using react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
 function App() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: () =>
+      fetch('https://wptavern.com/wp-json/wp/v2/posts').then((res) => res.json(),),
+  })
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
   return (
     <>
       <h1>Headpress</h1>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title.rendered}</li>
+        ))}
+      </ul>
+      <pre>{JSON.stringify(data[0], null, 2)}</pre>
     </>
   )
 }
